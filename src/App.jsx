2026057079af1aa97b279f6ccd0411e61fad2baf
Pooja Singh <amansiingh15@gmail.com>
2024-03-25 14:19:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Sucess from './pages/Sucess';
+import Error from './pages/Error';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element="Home"></Route>
+        <Route path='/sucess' element="Suces"></Route>
+        <Route path='/*' element={<Error/>}></Route>
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
