6fabf42254d871eefb68ecc62043077e91540085
Pooja Singh <amansiingh15@gmail.com>
2024-03-27 23:48:27 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,7 +13,7 @@ const Cart = () => {
         <Cartitems/>
         <Cartitems/>
         <div className='absolute bottom-0 mb-4'>
-            <h1>Items :</h1>
+            <h1>Items : you</h1>
             <h1>Amount :</h1>
             <hr />
             <button className='p-1 font-normal bg-green-500 rounded-lg text-white hover:bg-green-600 w-[90vw] lg:w-[23vw]'>Checkout</button>
