4b9b11de0ef40c9a007b4e8299d6c35d3fa81a66
Pooja Singh <amansiingh15@gmail.com>
2024-05-11 22:53:22 +0530

    Update

---
+++ src/redux/slices/CartSlice.jsx
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const CartSlice = createSlice({
+    name : "cart",
+    initialState : {
+        cart : [],
+    },
+
+    reducers : {
+
+        addToCart : () => {
+            
+        }
+    }
+
+    
+});
