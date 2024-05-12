c975c63a280562bed295e9cb23e4ea5e2700bb71
Pooja Singh <amansiingh15@gmail.com>
2024-05-12 16:57:37 +0530

    Add and remove from Cart

--- src/redux/slices/CartSlice.jsx
+++ src/redux/slices/CartSlice.jsx
@@ -8,8 +8,12 @@ const CartSlice = createSlice({
 
     reducers : {
 
-        addToCart : () => {
-            
+        addToCart : (state, action) => {
+            state.cart.push(action.payload);
+        },
+
+        removeFromCart : (state, action) => {
+            state.cart = state.cart.filter((item)=> item.id !=== action.payload.id);
         }
     }
 
